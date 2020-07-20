b2ea2529edee4019173d178918f4420c8fa9433e
Mdsomratakbor <somrat876@gmail.com>
2020-07-20 23:40:17 +0600

    SaveToFile method add

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModel.csv";
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            return model;
+           List<PrizeModel> prizes= PrizesFile.FullFilePath().LoadFile().ConvertPrizeModels();          
+            int currentId = prizes.OrderByDescending(x => x.Id).First().Id+1;
+            model.Id = currentId;
+            prizes.Add(model);
         }
-        public static List<string> LoadFile(this string file)
-        {
-
-        }
+       
     }
 }
