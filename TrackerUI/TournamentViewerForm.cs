170e8efd64e341c5a27479ada1577e52cfe02415
Mdsomratakbor <somrat876@gmail.com>
2020-08-10 23:53:10 +0600

    round dropdown data show is complete

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -14,15 +14,39 @@ namespace TrackerUI
     public partial class TournamentViewForm : Form
     {
         private TournamentModel tournament;
+        List<int> rounds = new List<int>();
         public TournamentViewForm(TournamentModel tornamentModel)
         {
             InitializeComponent();
             tournament = tornamentModel;
             LoadFormData();
+            LoadRounds();
         }
         private void LoadFormData()
         {
             tournamentName.Text = tournament.TournamentName;
+        }
+        private void WireUpLists()
+        {
+            roundDropDown.DataSource = null;
+            roundDropDown.DataSource = rounds;
+        }
+        private void LoadRounds()
+        {
+            rounds = new List<int>();
+            rounds.Add(1);
+            int currRound = 1;
+            foreach (List<MatchupModel> matchups in tournament.Rounds)
+            {
+                if (matchups.First().MatchupRound > currRound)
+                {
+                    currRound = matchups.First().MatchupRound;
+                    rounds.Add(currRound);
+                    currRound += 1;
+                }
+            }
+            WireUpLists();
+
         }
 
     }
